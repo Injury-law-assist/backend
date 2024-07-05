eca6336ddcc84dce99759dda746d1eec57b917ef
KNU-K <knukang334@gmail.com>
2024-07-05 22:21:49 +0900

    chore : coverage 보고서 생성 및 / workflows job 분리

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,27 @@
+// jest.config.js
+
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    reporters: [
+        'default',
+        [
+            'jest-html-reporters',
+            {
+                publicPath: './coverage',
+                filename: 'coverage-report.html',
+                expand: true,
+            },
+        ],
+    ],
 };
