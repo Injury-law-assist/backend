df38628f39c0df897112c9804680292afcbb2731
KNU-K <knukang334@gmail.com>
2024-07-06 22:53:54 +0900

    add : repo 및 기본 라우팅 구성 chat CRUD

--- src/api/controllers/chat.ts
+++ src/api/controllers/chat.ts
@@ -1,25 +1,46 @@
-import { NextFunction, Request, Response } from 'express';
+import { Application, NextFunction, Request, Response } from 'express';
 import ChatService from '../../services/chat';
 import { Inject, Service } from 'typedi';
+import error from '../middlewares/error';
 
 @Service()
 export default class ChatController {
     constructor(@Inject(() => ChatService) private readonly chatService: ChatService) {}
 
     getMessages = async (req: Request, res: Response, next: NextFunction) => {
-        const messages = await this.chatService.getMessages();
-        return res.status(200).json({ messages });
+        try {
+            const r_id = parseInt(req.params.r_id) as number;
+            const messages = await this.chatService.getMessages({ r_id });
+            return res.status(200).json({ messages });
+        } catch (err) {
+            return next(err);
+        }
     };
     getChatRoom = async (req: Request, res: Response, next: NextFunction) => {
-        const chatRooms = await this.chatService.getChatRooms();
-        return res.status(200).json({ chatRooms });
+        try {
+            const u_id = 1;
+            const chatRooms = await this.chatService.getChatRooms({ u_id });
+            return res.status(200).json({ chatRooms });
+        } catch (err) {
+            return next(err);
+        }
     };
-    joinChatRoom = async (req: Request, res: Response, next: NextFunction) => {
-        const newChatRoom = await this.chatService.joinChatRoom();
-        return res.status(200).json({ newChatRoom });
+    generateChatRoom = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const u_id = 1;
+            const newChatRoom = await this.chatService.generateChatRoom({ u_id });
+            return res.status(200).json({ newChatRoom });
+        } catch (err) {
+            return next(err);
+        }
     };
-    exitChatRoom = async (req: Request, res: Response, next: NextFunction) => {
-        await this.chatService.exitChatRoom();
-        return res.status(200).json({ msg: 'successfully deleted' });
+    deleteChatRoom = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const r_id = parseInt(req.params.r_id) as number;
+            await this.chatService.deleteChatRoom({ r_id });
+            return res.status(200).json({ msg: 'successfully deleted' });
+        } catch (err) {
+            return next(err);
+        }
     };
 }
--- src/api/routes/chat.ts
+++ src/api/routes/chat.ts
@@ -6,7 +6,7 @@ export default ({ app }: { app: Router }) => {
     const route = Router();
     app.use('/chat', route);
     route.get('/', Container.get(ChatController).getChatRoom.bind(ChatController));
-    route.post('/', Container.get(ChatController).joinChatRoom.bind(ChatController));
-    route.delete('/', Container.get(ChatController).exitChatRoom.bind(ChatController));
-    route.get('/messages', Container.get(ChatController).getMessages.bind(ChatController));
+    route.post('/', Container.get(ChatController).generateChatRoom.bind(ChatController));
+    route.delete('/:r_id', Container.get(ChatController).deleteChatRoom.bind(ChatController));
+    route.get('/:r_id/messages', Container.get(ChatController).getMessages.bind(ChatController));
 };
