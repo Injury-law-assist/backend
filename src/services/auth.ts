0e0b449ac1b9f63b97a9d03c993775eb3a386e89
KNU-K <knukang334@gmail.com>
2024-07-07 20:35:56 +0900

    fix : auth 인증 로직 추가

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,19 +1,24 @@
 import { Inject, Service } from 'typedi';
 import UserRepository from '../repositories/user';
 import { UserJoinRequestDTO, UserLoginRequestDTO } from '../dto/request/user';
-import { UserJoinResponseDTO, UserLoginResponseDTO } from '../dto/response/user';
+import { UserJoinResponseDTO, UserLoginResponseDTO, UserDTO } from '../dto/response/user';
 
+/**[ ] password encrypt 추가 */
 @Service()
 export default class AuthService {
     constructor(@Inject(() => UserRepository) private readonly userRepository: UserRepository) {}
+    generateToken() {
+        /**[ ] jwt 토큰 발행 로직 추가 */
+        return ['refreshToken', 'accessToken'];
+    }
     login = async ({ email, password }: UserLoginRequestDTO): Promise<UserLoginResponseDTO> => {
         const existingUser = await this.userRepository.findOneByPk({ email: email });
 
         if (!existingUser) throw new Error('존재하지않는 아이디입니다.');
+
         if (existingUser.u_password !== password) throw new Error('비밀번호가 일치하지않습니다.');
-        /**[ ] jwt 토큰 발행 로직 추가 */
-        /**[ ] password encrypt 추가 */
-        const [refreshToken, accessToken] = ['refreshToken', 'accessToken'];
+
+        const [refreshToken, accessToken] = this.generateToken();
         const userLoginResponseDTO: UserLoginResponseDTO = {
             statusCode: 200,
             message: '로그인에 성공하였습니다.',
@@ -21,9 +26,8 @@ export default class AuthService {
         };
         return userLoginResponseDTO;
     };
-    /** [ ]추후 타입 수정 필요 */
     join = async (user: UserJoinRequestDTO): Promise<UserJoinResponseDTO> => {
-        const existingUser = (await this.userRepository.findOneByPk({ email: user.email })) as any;
+        const existingUser = await this.userRepository.findOneByPk({ email: user.email });
         console.log(existingUser);
         if (existingUser) throw new Error('이미 존재하는 이메일입니다.');
         await this.userRepository.create(user);
