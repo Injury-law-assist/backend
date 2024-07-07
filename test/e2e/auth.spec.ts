a2140614c6693c4e4b9a1d1dba53879895f4f780
KNU-K <knukang334@gmail.com>
2024-07-07 17:25:36 +0900

    test : 커버리지 80%이상 & auth/error 테스트 진행 (e2e,unit)

--- test/e2e/auth.spec.ts
+++ test/e2e/auth.spec.ts
@@ -1,23 +1,74 @@
-import { Application } from "express";
-import request from "supertest";
-import createApp from "../../src/app";
+import { Application } from 'express';
+import request from 'supertest';
+import createApp from '../../src/app';
 
-describe("/api/auth test", () => {
+describe('/api/auth test', () => {
     let app: Application;
+    let email: any = null;
+    let password: any = null;
     beforeAll(async () => {
         app = await createApp();
     });
 
-    it("✅[GET /login] login", async () => {
-        const res = await request(app).post("/api/auth/login");
+    it('✅ [POST /auth/join] join', async () => {
+        // 현재 날짜와 시간을 포함한 이메일 생성
+        const currentDate = new Date()
+            .toISOString()
+            .replace(/[-T:.]/g, '')
+            .slice(0, 14);
+        email = `testuser_${currentDate}@example.com`;
+
+        const userData = {
+            email: email,
+            password: 'password123',
+            nickname: 'TestUser',
+        };
+        password = userData.password;
+        const res = await request(app).post('/api/auth/join').send(userData);
+
         expect(res.statusCode).toBe(200);
-        expect(res.body).toHaveProperty("msg");
-        expect(res.body.msg).toBe("login");
+        expect(res.body).toHaveProperty('user');
+        expect(res.body.user).toHaveProperty('u_id');
+        expect(res.body.user).toHaveProperty('u_email', userData.email);
+        expect(res.body.user).toHaveProperty('u_nickname', userData.nickname);
+        expect(res.body.user).toHaveProperty('u_password'); // 비밀번호는 반환되면 안 됩니다 (일반적으로).
+        expect(res.body.user).toHaveProperty('u_created_at');
+        expect(res.body.user).toHaveProperty('u_updated_at');
     });
-    it("✅[GET /join] join", async () => {
-        const res = await request(app).post("/api/auth/join");
+
+    it('✅[GET /login] login - 정상', async () => {
+        const res = await request(app).post('/api/auth/login').send({
+            email: email,
+            password: password,
+        });
         expect(res.statusCode).toBe(200);
-        expect(res.body).toHaveProperty("msg");
-        expect(res.body.msg).toBe("join");
+        expect(res.body).toHaveProperty('msg');
+        expect(res.body.msg).toBe('token');
+    });
+
+    it('✅[GET /login] login - 아이디 존재x', async () => {
+        const res = await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: email + 'asdasdasd',
+                password: password,
+            });
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toHaveProperty('message');
+        expect(res.body).toHaveProperty('error');
+        expect(res.body.error).toBe('존재하지않는 아이디입니다.');
+    });
+
+    it('✅[GET /login] login - 비밀번호 불일치', async () => {
+        const res = await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: email,
+                password: password + 'ㅁㅁ',
+            });
+        expect(res.statusCode).toBe(500);
+        expect(res.body).toHaveProperty('message');
+        expect(res.body).toHaveProperty('error');
+        expect(res.body.error).toBe('비밀번호가 일치하지않습니다.');
     });
 });
