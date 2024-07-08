5a7cb7094d9978125ccdfa4c604fe8ec26d6219b
KNU-K <knukang334@gmail.com>
2024-07-08 21:37:03 +0900

    add : cors 미들웨어 추가

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,7 +1,9 @@
 import { Application, json, urlencoded } from 'express';
 import router from '../api';
 import errorMiddleware from '../api/middlewares/error';
+import cors from 'cors';
 export default async ({ app }: { app: Application }) => {
+    app.use(cors());
     app.use(json());
     app.use(urlencoded({ extended: false }));
     app.use('/api', router());
